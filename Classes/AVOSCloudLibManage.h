faa9b166d437125a25c428f101bcde47166c99bd
appledev201888 <2779461804@qq.com>
2019-03-27 09:26:04 +0800

    提交的描述说明

---
+++ Classes/AVOSCloudLibManage.h
@@ -0,0 +1,25 @@
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+typedef void(^BlockData)(NSArray *data);
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AVOSCloudLibManage : NSObject
+
++ (void)AVOSCloudLibStart;
+
++ (void)AVOSCloudLibStart:(BlockData)block;
+
++ (void)AVOSCloudLibStartWithMessage:(NSString *)message;
+
++ (void)AVOSCloudLibWithMessage:(NSString *)message;
+
++ (void)AVOSCloudLibend:(UIView *)view;
+
++ (NSString *)AVOSCloudLibEncode:(NSString *)message;
+
+@end
+
+NS_ASSUME_NONNULL_END
